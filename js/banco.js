d603ce46c666763a57e5d0552e664c4994a972be
Erisvam <erisvamh@gmail.com>
2020-10-11 18:16:26 -0300

    login completo

--- js/banco.js
+++ js/banco.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyAVLD618EUY3O7ssLEDc-n3ATwpTH8jA_c",
     authDomain: "smartplug-7d741.firebaseapp.com",
     databaseURL: "https://smartplug-7d741.firebaseio.com/",
@@ -10,14 +10,14 @@ const firebaseConfig = {
 };
 
 // var firebaseConfig = {
-//     apiKey: "AIzaSyCLvPHVICq6C8kpYS-Zsw1Dfb-pDSWyCSc",
-//     authDomain: "teste-tcc-bb2d9.firebaseapp.com",
-//     databaseURL: "https://teste-tcc-bb2d9.firebaseio.com",
-//     projectId: "teste-tcc-bb2d9",
-//     storageBucket: "teste-tcc-bb2d9.appspot.com",
-//     messagingSenderId: "71678916346",
-//     appId: "1:71678916346:web:05f00df680e5e5d5cbfb43",
-//     measurementId: "G-RBLNWGD3CB"
+//     apiKey: "AIzaSyAhj8kpn35PrelSPJguOFZ0uosNBJ-UMoI",
+//     authDomain: "neon-fce00.firebaseapp.com",
+//     databaseURL: "https://neon-fce00.firebaseio.com",
+//     projectId: "neon-fce00",
+//     storageBucket: "neon-fce00.appspot.com",
+//     messagingSenderId: "464013235447",
+//     appId: "1:464013235447:web:374f2d8b7c2507aa5eaff2",
+//     measurementId: "G-16BF6G5QCK"
 // };
 
 // Initialize Firebase
@@ -26,30 +26,32 @@ firebase.analytics();
 
 
 //gerador de IDs 
-let date = new Date();
-var time = date.getTime();
+// let date = new Date();
+// var time = date.getTime();
 
 //cadastrar nome e senha do roteador
-function registerUser() {
-    time += 1;
+// function registerUser() {
+//     time += 1;
 
-    let routerName = $("#search").val();
-    let routerPasswd = $("#passwd").val();
+//     let routerName = $("#search").val();
+//     let routerPasswd = $("#passwd").val();
 
-    if (routerName && routerPasswd) {
-        var user = {
-            id: time.toString(),
-            description: routerName,
-            password: routerPasswd
-        }
-        let banco = firebase.database().ref('router/');
-        banco.set(user);
-    } else {
-        // put on message of error
-    }
-}
+//     if (routerName && routerPasswd) {
+//         var user = {
+//             id: time.toString(),
+//             description: routerName,
+//             password: routerPasswd
+//         }
+//         let banco = firebase.database().ref('router/');
+//         banco.set(user);
+//     } else {
+//         // put on message of error
+//     }
+// }
+
 
 var response;
+readDataBase()
 
 function read() {
     const banco = firebase.database().ref();
@@ -57,11 +59,21 @@ function read() {
     banco.on("value", (snapshot) => {
         response = snapshot.val();
 
-        console.log(Object.values(response.LDR)[Object.values(response.LDR).length - 1]);
+        // let getUsuario = response.usuario;
+        // let getDispositivos = response.dispositivos
 
-        for (let index = 0; index < Object.values(response.LDR).length; index++) {
-            console.log(Object.values(response.LDR)[index]);
-        }
+        // let titleCard = $("#titleCard");
+        // let timer = $("#timer");
+        // for (const iterator of getDispositivos) {
+        //     $(titleCard).html(iterator.nomeDispositivo);
+        //     $(timer).html(`Temporizador: <strong>${iterator.temporizador} minuto</strong>`);
+        // }
+
+        // console.log(Object.values(response.LDR)[Object.values(response.LDR).length - 1]);
+
+        // for (let index = 0; index < Object.values(response.LDR).length; index++) {
+        //     console.log(Object.values(response.LDR)[index]);
+        // }
 
     });
 }
@@ -103,3 +115,147 @@ $(".toast-body button").click(function() {
         });
     }
 });
+
+var valueNameRoteador;
+var valuePasswordRoteador;
+var valuePasswordAccess;
+
+const FIELD_OK = true;
+const FIELD_ERROR = false;
+const FIELD_INVALID = "fildInvalid";
+const FIELD_VALID = "fildValid";
+const STRING_EMPTY = "";
+const ATTR_HREF = "href";
+const PAGINA_INDEX = "index.html"
+const userDate = {};
+
+// REGISTER USER
+const registerUser = function() {
+    const registerValid = validateUserRegistration().every(function() {
+        return FIELD_OK;
+    });
+
+    if (registerValid) {
+        var user = {
+            id: 2,
+            nomeWifi: valueNameRoteador,
+            senhaWifi: valuePasswordRoteador,
+            senhaDeAcesso: valuePasswordAccess
+        }
+        let banco = firebase.database().ref('/usuario');
+        banco.set(user);
+        clearModalAndClose(".close");
+        setTimeout(function() {
+            showMessageSuccess();
+        }, 600);
+    }
+}
+
+function readDataBase() {
+    const bank = firebase.database().ref();
+
+    bank.on("value", (snapshot) => {
+        response = snapshot.val();
+        readUser(response);
+    });
+}
+
+
+const readUser = response => {
+    userDate.nameWifi = response.usuario.nomeWifi;
+    userDate.passwordAccess = response.usuario.senhaDeAcesso;
+}
+
+const logIn = function() {
+    let userLogin = $("#userLogin").val();
+    let messageErrorUserLogin = $("#userLoginError");
+
+    let passwordAccess = $("#passwordAccess").val();
+    let messageErrorPasswordAccess = $("#passwordAccessError");
+
+    let checkingFields = [isNotEmpty(userLogin, messageErrorUserLogin), isNotEmpty(passwordAccess, messageErrorPasswordAccess)]
+
+    let nameWifiRight = fildRightOrError((userDate.nameWifi === userLogin), messageErrorUserLogin);
+    let passwordAccessRight = fildRightOrError((userDate.passwordAccess === passwordAccess), messageErrorPasswordAccess);
+
+    const valid = checkingFields.every(() => {
+        if (nameWifiRight && passwordAccessRight) {
+            return FIELD_OK;
+        }
+    });
+
+    if (valid) {
+        $("#btn-entrar").attr(ATTR_HREF, PAGINA_INDEX);
+    }
+}
+
+const isNotEmpty = (fieldValue, fieldError) => {
+    if (fieldValue !== STRING_EMPTY) {
+        $(fieldError).removeClass(FIELD_INVALID);
+        $(fieldError).addClass(FIELD_VALID);
+        return FIELD_OK;
+    } else {
+        $(fieldError).addClass(FIELD_INVALID);
+        return FIELD_ERROR;
+    }
+}
+const clearModalAndClose = (classNameModal) => {
+    clearField("#nameWifi");
+    clearField("#passwordWifi");
+    clearField("#passwordAcess");
+
+    setTimeout(() => $(classNameModal).click(), 200);
+}
+
+const validateUserRegistration = () => {
+
+    valueNameRoteador = $("#nameWifi").val();
+    let messageErrorNameRoteador = $("#nameWifiError");
+
+    valuePasswordRoteador = $("#passwordWifi").val();
+    let messageErrorPasswordRoteador = $("#passwordWifiError");
+
+    valuePasswordAccess = $("#passwordAcess").val();
+    let messageErrorPasswordAccess = $("#passwordAcessError");
+
+    let checkingFields = [
+        isNotEmpty(valueNameRoteador, messageErrorNameRoteador),
+        isNotEmpty(valuePasswordRoteador, messageErrorPasswordRoteador),
+        isNotEmpty(valuePasswordAccess, messageErrorPasswordAccess)
+    ]
+    return checkingFields;
+}
+
+const clearField = field => $(field).val(STRING_EMPTY);
+
+const fildRightOrError = (condicional, messageError) => {
+    if (condicional) {
+        $(messageError).removeClass(FIELD_INVALID);
+        $(messageError).addClass(FIELD_VALID);
+        return FIELD_OK;
+    } else {
+        $(messageError).addClass(FIELD_INVALID);
+        return FIELD_ERROR;
+    }
+}
+const showMessageSuccess = () => {
+    setTimeout(() => {
+        animation("top");
+    }, 2000);
+
+    $(".messageSuccess").show();
+    animation("bottom");
+}
+
+function animation(side) {
+    $(".messageSuccess__alertSuccess").css({
+        "animation-name": `slide-${side}`,
+        "animation-duration": "2s"
+    });
+
+    if (side === "top") {
+        setTimeout(function() {
+            $(".messageSuccess").hide();
+        }, 2000);
+    }
+}
